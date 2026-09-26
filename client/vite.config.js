101abc737da5eb2756ec6a5d9b22e8db3834c952
gowthamgokul135-blip <gowthamgokul135@gmail.com>
2026-09-26 11:10:23 +0530

    Configure GitHub Pages base

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
 
   base:" ",
 
-  // server: {
-  //   port: 5173,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8000',
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
 });
