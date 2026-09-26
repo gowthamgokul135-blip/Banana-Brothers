e961a5de9b6b1bf3387794bd51c804036d66fe62
gowthamgokul135-blip <gowthamgokul135@gmail.com>
2026-09-26 11:06:15 +0530

    Configure GitHub Pages base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  // base: '',
+  base:" ",
 
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
-      }
-    }
-  }
+  // server: {
+  //   port: 5173,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 });
